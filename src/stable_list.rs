452cfc2a8cb3bd1246376a1dbfb08133e7fda967
Alex Addy <alex.addy@gmail.com>
2021-05-03 15:24:08 -0400

    Close to resolving list issues

--- src/stable_list.rs
+++ src/stable_list.rs
@@ -1,62 +1,86 @@
 use std::collections::LinkedList;
 use std::mem::MaybeUninit;
+use std::cell::UnsafeCell;
+use std::sync::{RwLock, atomic::{AtomicU32, Ordering}};
 
 const BLOCK_SIZE: usize = 128;
 
 pub struct StableList<T> {
-    // TODO do I need to use a LinkedList, can I use a Vec and only protect access to it with all
-    // iterators caching their current array?
-    list: LinkedList<Box<[MaybeUninit<T>; BLOCK_SIZE]>>,
+    // TODO check that linkedlist is stable upon push
+    list_lock: RwLock<LinkedList<[UnsafeCell<MaybeUninit<T>>; BLOCK_SIZE]>>,
     /// Index just past the last initialized item in the StableList
     ///
     /// The item pointed to by this idx is uninitialized or may not exist.
-    last_global_idx: usize,
+    last_global_idx: AtomicU32,
 }
 
 impl<T> StableList<T> {
     pub fn new() -> Self {
         StableList{
-            list: LinkedList::new(),
-            last_global_idx: 0,
+            list_lock: RwLock::new(LinkedList::new()),
+            last_global_idx: AtomicU32::new(0),
         }
     }
 
-    pub fn push(&mut self, item: T) {
-       if self.last_global_idx % BLOCK_SIZE == 0 {
-            // we have all full blocks and have to add a new one
-            self.add_block();
+    pub fn push(&self, item: T) {
+       let mut list = match self.list_lock.write() {
+           Ok(lock) => lock,
+           Err(_) => panic!("StableList's internal mutex has been poisoned"),
+       };
+       // make sure to get the most recent value, don't move this before the lock
+       let global_idx = self.last_global_idx.load(Ordering::SeqCst) as usize;
+       if global_idx != u32::MAX as usize {
+           panic!("list is full, cannot index past 2^32");
        }
-       let last_block = self.list.iter_mut().last().expect("no block in list even though we called add_block");
-       let idx = self.last_global_idx % BLOCK_SIZE;
-       last_block[idx] = MaybeUninit::new(item);
-       self.last_global_idx += 1;
+       if global_idx % BLOCK_SIZE == 0 {
+            // we have all full blocks and have to add a new one
+            // Safety: We are telling the compiler to assume initialization of the MaybeUninit values
+            // *not* the T inside them. MaybeUninit requires no initialization.
+            let block: [UnsafeCell<MaybeUninit<T>>; BLOCK_SIZE] = unsafe { MaybeUninit::uninit().assume_init() };
+            list.push_back(block);
+       }
+       let last_block = list.iter_mut().last().expect("no block in list even though we tried to add one");
+       last_block[global_idx % BLOCK_SIZE] = UnsafeCell::new(MaybeUninit::new(item));
+       // Safety: only modify last_global_idx while we have the lock
+       self.last_global_idx.fetch_add(1, Ordering::SeqCst);
     }
 
     pub fn len(&self) -> usize {
-       self.last_global_idx 
+       self.last_global_idx.load(Ordering::SeqCst) as usize
     }
 
-    fn add_block(&mut self) {
-        // Safety: We are telling the compiler to assume initialization of the MaybeUninit values
-        // *not* the T inside them. MaybeUninit requires no initialization.
-        let block: [MaybeUninit<T>; BLOCK_SIZE] = unsafe { MaybeUninit::uninit().assume_init() };
-        self.list.push_back(Box::new(block));
+    unsafe fn get_chunk(&self, idx: usize) -> Option<&[UnsafeCell<MaybeUninit<T>>; BLOCK_SIZE]> {
+        match self.list_lock.read() {
+            Ok(lock) => lock.iter().take(idx).last(),
+            Err(_) => panic!("StableList's internal mutex has been poisoned"),
+        }
     }
 
     fn get(&self, idx: usize) -> Option<&T> {
-        if idx < self.last_global_idx {
-            // Safety: All values less before last_global_idx are guaranteed to be initialized
-            self.list.iter().take(idx / BLOCK_SIZE + 1).last().map(|ch| unsafe { &*ch[idx % BLOCK_SIZE].as_ptr() })
+        if idx < self.last_global_idx.load(Ordering::SeqCst) as usize {
+            let list = match self.list_lock.read() {
+               Ok(lock) => lock,
+               Err(_) => panic!("StableList's internal mutex has been poisoned"),
+            };
+            // Safety: All values before last_global_idx are guaranteed to be initialized
+            list.iter().take(idx / BLOCK_SIZE + 1)
+                .last()
+                .map(|ch| unsafe { &*(*ch[idx % BLOCK_SIZE].get()).as_ptr()} )
         } else {
             None
         }
     }
 
     fn iter(&self) -> StableListIterator<'_, T> {
+        let list = match self.list_lock.read() {
+           Ok(lock) => lock,
+           Err(_) => panic!("StableList's internal mutex has been poisoned"),
+        };
+        let global_idx = self.last_global_idx.load(Ordering::SeqCst) as usize;
         StableListIterator {
-            global_idx: self.last_global_idx,
-            chunk_idx: self.last_global_idx % BLOCK_SIZE,
-            chunk: self.list.iter().last(),
+            global_idx: global_idx,
+            chunk_idx: global_idx % BLOCK_SIZE,
+            chunk: list.iter().last(),
             list: self,
         }
     }
@@ -68,7 +92,7 @@ impl<T> StableList<T> {
 struct StableListIterator<'a, T: 'a> {
     global_idx: usize,
     chunk_idx: usize,
-    chunk: Option<&'a Box<[MaybeUninit<T>; BLOCK_SIZE]>>,
+    chunk: Option<&'a [UnsafeCell<MaybeUninit<T>>; BLOCK_SIZE]>,
     list: &'a StableList<T>,
 }
 
@@ -76,7 +100,7 @@ impl<'a, T> Iterator for StableListIterator<'a, T> {
     type Item = &'a T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.global_idx == self.list.last_global_idx {
+        if self.global_idx == self.list.last_global_idx.load(Ordering::SeqCst) as usize {
             // no more values to return
             return None;
         }
@@ -84,19 +108,20 @@ impl<'a, T> Iterator for StableListIterator<'a, T> {
         if self.chunk_idx + 1 == BLOCK_SIZE {
             // this would be a lot simpler if LinkedList exposed a way to hold a reference to a
             // node, the proposed cursor API might be what is necessary: https://github.com/rust-lang/rust/issues/58533
-            match self.list.list.iter().take(self.global_idx % BLOCK_SIZE + 1).last() {
+            // TODO safety
+            match unsafe { self.list.get_chunk(self.global_idx / BLOCK_SIZE + 1) } {
                 None => return None,
                 Some(chunk) => {
                     self.chunk_idx = 0;
                     self.global_idx += 1;
                     self.chunk = Some(chunk);
-                    return self.chunk.map(|ch| unsafe { &*ch[self.chunk_idx].as_ptr()} )
+                    return self.chunk.map(|ch| unsafe { &*(*ch[self.chunk_idx].get()).as_ptr()} )
                 },
             }
         }
         self.chunk_idx += 1;
         self.global_idx += 1;
-        return self.chunk.map(|ch| unsafe { &*ch[self.chunk_idx].as_ptr()} )
+        return self.chunk.map(|ch| unsafe { &*(*ch[self.chunk_idx].get()).as_ptr()} )
     }
 }
 
@@ -108,7 +133,7 @@ mod test {
     #[test]
     // Test that an item pushed into StableList can then be retrieved via `get`
     fn push_and_check_single_item() {
-        let mut list = StableList::new();
+        let list = StableList::new();
         assert_eq!(list.get(0), None);
         list.push(0xABABi32);
         assert_eq!(list.get(0), Some(&0xABAB));
@@ -117,7 +142,7 @@ mod test {
     #[test]
     // Test that populating the list then iterating over it works
     fn populate_and_iterate_simple() {
-        let mut list = StableList::new();
+        let list = StableList::new();
         let mut iter = list.iter();
         for i in 0..(BLOCK_SIZE*5) {
             list.push(i);
