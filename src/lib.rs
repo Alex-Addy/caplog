8531e0b4cfad65a29cb9fcaeb036d387b6437939
Alex Addy <alex.addy@gmail.com>
2021-06-29 10:13:25 -0400

    Restrict CaplogHandle's log access
    Will only operate on logs created while it was alive.

--- src/lib.rs
+++ src/lib.rs
@@ -45,23 +45,51 @@ pub struct Record {
     pub msg: String,
 }
 
+/// Provides access to the logs stored in Caplog.
+///
+/// Access is limited to the time the handle has been alive. Log messages sent before handle has
+/// been created and after `stop_recording` have been called will not be visible to the methods
+/// provided by CaplogHandle.
 pub struct CaplogHandle {
+    start_idx: usize,
+    stop_idx: Option<usize>,
     list: Arc<StableList<Record>>,
 }
 
 impl CaplogHandle {
     pub fn any_msg_contains(&self, snippet: &str) -> bool {
-        self.list.iter().any(|rec| rec.msg.contains(snippet))
+        self.list.bounded_iter(self.start_idx, self.stop_idx).any(|rec| rec.msg.contains(snippet))
     }
 
-    pub fn iter(&self) -> impl Iterator + '_ {
-        self.list.iter()
+    /// Returns an iterator over the items viewable by this handle.
+    pub fn iter(&self) -> crate::stable_list::StableListIterator<Record> {
+        // TODO remove StableListIterator type from exposed types
+        self.list.bounded_iter(self.start_idx, self.stop_idx)
+    }
+
+    pub fn stop_recording(&mut self) {
+        self.stop_idx = Some(self.list.len());
     }
 }
 
+/// Get a handle to the recorded logs. Handle is restricted to only viewing the logs available
+/// while it is alive.
+///
+/// # Example
+/// ```rust
+/// # use log::{info, warn};
+/// info!("not recorded");
+/// let handle = caplog::get_handle();
+/// info!("recorded");
+/// assert!(handle.iter().any(|rec| rec.msg.contains("not recorded")) == false);
+/// assert!(handle.any_msg_contains("recorded"));
+/// ```
 pub fn get_handle() -> CaplogHandle {
+    let log_list = _CAPTURE_LOG.logs.clone();
     CaplogHandle {
-        list: _CAPTURE_LOG.logs.clone(),
+        start_idx: log_list.len(),
+        stop_idx: None,
+        list: log_list.clone(),
     }
 }
 
